1e466183813d9581ceb348c03fb15b0f07495fdf
David Avedissian <git@davedissian.com>
2016-01-16 20:33:14 +0000

    Builds correctly on OS X

--- src/dawn/renderer/OSXGetNSView.mm
+++ src/dawn/renderer/OSXGetNSView.mm
@@ -1,6 +1,6 @@
 /*
  * Dawn Engine
- * Written by David Avedissian (c) 2012-2015 (avedissian.david@gmail.com)
+ * Written by David Avedissian (c) 2012-2016 (git@davedissian.com)
  */
 #include "Common.h"
 #include "OSXGetNSView.h"
@@ -11,9 +11,9 @@ NAMESPACE_BEGIN
 
 uintptr_t getWindowContentViewHandle(SDL_SysWMinfo &info)
 {
-  NSWindow *window = info.info.cocoa.window;
-  NSView *view = [window contentView];
-  return (uintptr_t)view;
+    NSWindow *window = info.info.cocoa.window;
+    NSView *view = [window contentView];
+    return (uintptr_t)view;
 }
 
 NAMESPACE_END
