540369e33bceeb50deffd339aaeaff8a26a2ede9
David Avedisisan <git@davedissian.com>
2016-07-29 17:52:29 +0100

    Added timing functions

---
+++ src/dawn/core/Timer.cpp
@@ -0,0 +1,35 @@
+/*
+ * Dawn Engine
+ * Written by David Avedissian (c) 2012-2016 (git@davedissian.com)
+ */
+#include "Common.h"
+#include "core/Timer.h"
+
+NAMESPACE_BEGIN
+namespace time {
+
+TimePoint beginTiming()
+{
+    return _SteadyClock::now();
+}
+
+double elapsed(TimePoint then)
+{
+    return std::chrono::duration_cast<std::chrono::seconds>(_SteadyClock::now() - then).count();
+}
+
+SystemTimePoint now()
+{
+    return _SystemClock::now();
+}
+
+String format(SystemTimePoint time, const String& format)
+{
+    StringStream out;
+    std::time_t tt = _SystemClock::to_time_t(time);
+    out << std::put_time(std::gmtime(&tt), format.c_str());
+    return out.str();
+}
+
+}
+NAMESPACE_END
---
+++ src/dawn/core/Timer.h
@@ -0,0 +1,40 @@
+/*
+ * Dawn Engine
+ * Written by David Avedissian (c) 2012-2016 (git@davedissian.com)
+ */
+#pragma once
+
+namespace dw {
+namespace time {
+
+using _SystemClock = std::chrono::system_clock;
+using _SteadyClock = std::chrono::steady_clock;
+using TimePoint = _SteadyClock::time_point;
+using SystemTimePoint = _SystemClock::time_point;
+template <class T> using Duration = std::chrono::duration<double, std::chrono::seconds>;
+
+using std::chrono::nanoseconds;
+using std::chrono::microseconds;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::minutes;
+using std::chrono::hours;
+
+// Intervals
+
+// Return the current steady time to be used for interval calculations
+DW_API TimePoint beginTiming();
+
+// Return the time elapsed in seconds since a time point to when this function was called
+DW_API double elapsed(TimePoint then);
+
+// Calendar
+
+// return the current system time
+DW_API SystemTimePoint now();
+
+// Format a timepoint as a string
+DW_API String format(SystemTimePoint time, const String& format);
+
+}
+}
