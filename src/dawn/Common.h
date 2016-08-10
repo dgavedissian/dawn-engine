b2368e4c183b89d21d58468df7fdab43c303b78a
David Avedisisan <git@davedissian.com>
2016-08-10 18:59:02 +0100

    Renamed int# to i# and uint# to u# to match Rust

--- src/dawn/Common.h
+++ src/dawn/Common.h
@@ -20,14 +20,15 @@ namespace dw {
 
 // Primitive types
 using uint = unsigned int;
-using int8 = int8_t;
-using uint8 = uint8_t;
-using int16 = int16_t;
-using uint16 = uint16_t;
-using int32 = int32_t;
-using uint32 = uint32_t;
-using int64 = int64_t;
-using uint64 = uint64_t;
+using i8 = int8_t;
+using u8 = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+using byte = u8;
 
 // String
 using String = std::string;
