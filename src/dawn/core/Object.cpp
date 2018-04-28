285ffa30e6fbc53245238d061507c3ae12b82229
David Avedissian <git@dga.me.uk>
2018-04-28 10:06:37 +0100

    Update year to 2018.

--- src/dawn/core/Object.cpp
+++ src/dawn/core/Object.cpp
@@ -1,6 +1,6 @@
 /*
  * Dawn Engine
- * Written by David Avedissian (c) 2012-2017 (git@dga.me.uk)
+ * Written by David Avedissian (c) 2012-2018 (git@dga.me.uk)
  */
 #include "Common.h"
 #include "core/StringUtils.h"
