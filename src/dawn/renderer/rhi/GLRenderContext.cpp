e91a10c1bfdc7a9be9b381e462891fb81014aa85
David Avedissian <git@dga.me.uk>
2019-01-29 23:54:30 +0000

    Merge test dir into src dir.

--- src/dawn/renderer/rhi/GLRenderContext.cpp
+++ src/dawn/renderer/rhi/GLRenderContext.cpp
@@ -355,7 +355,7 @@ Result<void> GLRenderContext::createWindow(u16 width, u16 height, const String& 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #elif !defined(DW_EMSCRIPTEN)
 #error Unsupported: GLES 3.0 on non Web platform.
 #endif
