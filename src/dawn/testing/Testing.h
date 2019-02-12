a72aed20fba5ff28b56b0d6e7bdf54ab62f9d534
David Avedissian <git@dga.me.uk>
2019-02-12 23:00:55 +0000

    Add missing testing file.

---
+++ src/dawn/testing/Testing.h
@@ -0,0 +1,12 @@
+/*
+ * Dawn Engine
+ * Written by David Avedissian (c) 2012-2018 (git@dga.me.uk)
+ */
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include "Common.h"
+#include "core/Context.h"
+#include "core/Log.h"
+#include "core/io/FileSystem.h"
